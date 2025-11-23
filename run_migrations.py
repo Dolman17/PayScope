e28e48a0d0c0fbc71b8c2d1323ec9c0ea0466383
Dolman17 <seandolman17@gmail.com>
2025-11-23 14:37:59 +0000

    Update

---
+++ run_migrations.py
@@ -0,0 +1,9 @@
+from app import create_app
+from extensions import db
+from flask_migrate import upgrade
+
+app = create_app()
+
+with app.app_context():
+    upgrade()
+    print("✅ Database migrated.")
