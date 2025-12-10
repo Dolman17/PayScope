e58ea63bc42fa240e598ce444bb8479f2af14c72
Dolman17 <seandolman17@gmail.com>
2025-12-10 23:25:53 +0000

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -67,7 +67,7 @@ def create_app():
     from .blueprints.upload import bp as upload_bp
     from .blueprints.main import bp as main_bp
     from .blueprints.api import bp as api_bp      # ⬅️ NEW IMPORT
-    from blueprints.marketing import marketing_bp
+    from .blueprints.marketing import marketing_bp
 
     app.register_blueprint(main_bp)
     app.register_blueprint(auth_bp)
